0ae616e7e32a46bb00250748e305e4743f815796
Frederik Vig <frederik@geta.no>
2015-04-22 15:37:11 +0200

    Added description of package

--- Geta.EPi.Tracking.Commerce/Properties/AssemblyInfo.cs
+++ Geta.EPi.Tracking.Commerce/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Geta.EPi.Tracking.Commerce")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Geta Foundation Commerce Tracking")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Geta")]
 [assembly: AssemblyProduct("Geta.EPi.Tracking.Commerce")]
