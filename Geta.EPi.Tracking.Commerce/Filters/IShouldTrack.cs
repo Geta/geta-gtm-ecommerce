03983b6cf79a7617b8c6c342b22b9a5f926d3dce
Mari Jørgensen <mari@geta.no>
2017-01-12 15:21:51 +0100

    Removing old project

--- Geta.EPi.Tracking.Commerce/Filters/IShouldTrack.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Geta.EPi.Tracking.Commerce.Filters
-{
-    /// <summary>
-    /// Marker interface to track the request using TrackingFilterAttribute
-    /// </summary>
-    public interface IShouldTrack
-    {
-    }
-}
